8fd8e8366febc41ed329840991dabd0d51849326
kyoungho.moon <moonpeter@kakao.com>
2020-08-14 14:32:26 +0900

    WebClient

--- src/main/java/com/tutorialspoint/demo/RestRunner.java
+++ src/main/java/com/tutorialspoint/demo/RestRunner.java
@@ -3,34 +3,49 @@ package com.tutorialspoint.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StopWatch;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Component
 public class RestRunner implements ApplicationRunner {
 
     @Autowired
-    private RestTemplateBuilder restTemplateBuilder;
-//    private RestTemplate restTemplate;
+    WebClient.Builder builder;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        RestTemplate = restTemplate = restTemplateBuilder.build();  // build를 하면 RestTemplate가 나온다.
-
+        WebClient webClient = builder.build();
         StopWatch stopWatch = new StopWatch();
         stopWatch.start();
 
         // TODO '/hello' 호출
-        String helloResult = restTemplate.getForObject("http://localhost:8080/hello", String.class); // 두 번째 인자는 원하는 응답의 타입
-        System.out.println(helloResult);
+        Mono<String> helloMono = webClient.get().uri("http://localhost:80/hello")
+                .retrieve() // 결과 값을 가져와라
+                .bodyToMono(String.class); // 모노 타입으로 변경해라
+        helloMono.subscribe(s->{
+            System.out.println(s);
+
+            if(stopWatch.isRunning()) {
+                stopWatch.stop();
+            }
+            System.out.println(stopWatch.prettyPrint());
+            stopWatch.start(); // 어떤 것이 먼저 끝날지 모르므로, StopWatch를 다시 돌려줘야함
+        });
 
         // TODO '/world' 호출
-        String worldResult = restTemplate.getForObject("http://localhost:8080/world", String.class);
-        System.out.println(worldResult);
+        Mono<String> worldMono = webClient.get().uri("http://localhost:80/world")
+                .retrieve()
+                .bodyToMono(String.class);
+        worldMono.subscribe(s->{
+            System.out.println(s);
 
-        stopWatch.stop();
-        System.out.println(stopWatch.prettyPrint());
+            if(stopWatch.isRunning()) {
+                stopWatch.stop();
+            }
+            System.out.println(stopWatch.prettyPrint());
+            stopWatch.start(); // 어떤 것이 먼저 끝날지 모르므로, StopWatch를 다시 돌려줘야함
+        });
     }
 }
